0f86963a1da8061db8ce2fc7b7a22882070fdb1d
rainbow494 <rainbow494@gmail.com>
2016-08-24 06:09:45 +0800

    feat(sync lunch-wx-api) update sync-script

--- src/sync-lunch-wx-api.bat
+++ src/sync-lunch-wx-api.bat
@@ -1,1 +1,5 @@
 git -C f:\github\lunch-wx-api-clone pull
+
+cd f:\github\lunch-wx-api-clone
+call npm install
+call npm run build-dev
--- src/sync-lunch-wx-api.sh
+++ src/sync-lunch-wx-api.sh
@@ -1,1 +1,5 @@
 git -C /home/ubuntu/paul/lunch-wx-api pull
+
+cd /home/ubuntu/paul/lunch-wx-api
+call npm install
+call npm run build-dev
