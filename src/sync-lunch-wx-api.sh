e1f373bc0e22e81bc82700f975d2a975886b6f5a
rainbow494 <rainbow494@gmail.com>
2016-08-27 13:44:46 +0800

    update script

--- src/sync-lunch-wx-api.sh
+++ src/sync-lunch-wx-api.sh
@@ -1,10 +1,12 @@
+forever stop /home/ubuntu/paul/lunch-wx-api/build_product/server.js
+
 git -C  /home/ubuntu/paul/lunch-wx-api/ pull 
+
 cd /home/ubuntu/paul/lunch-wx-api/
-
 npm install
-
 npm run build-prod
 
 cd /home/ubuntu/paul/lunch-wx-api/build_product/
-
 npm install
+
+forever start /home/ubuntu/paul/lunch-wx-api/build_product/server.js
